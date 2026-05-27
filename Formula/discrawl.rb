955f6e6e3880fa269c7ecad41a00e8badd34b869
Vincent Koc <vincentkoc@ieee.org>
2026-05-27 21:18:06 +0100

    discrawl: fix formula syntax for v0.10.0

--- Formula/discrawl.rb
+++ Formula/discrawl.rb
@@ -6,21 +6,21 @@ class Discrawl < Formula
 
   on_macos do
     if Hardware::CPU.arm?
-     url "https://github.com/openclaw/discrawl/releases/download/v0.10.0/discrawl_0.10.0_darwin_arm64.tar.gz"
-      sha256 "64203c3e1d01b9c9369263e41a779b699f8b45978f18448599f8a18004ee2527""
+      url "https://github.com/openclaw/discrawl/releases/download/v0.10.0/discrawl_0.10.0_darwin_arm64.tar.gz"
+      sha256 "64203c3e1d01b9c9369263e41a779b699f8b45978f18448599f8a18004ee2527"
     else
-     url "https://github.com/openclaw/discrawl/releases/download/v0.10.0/discrawl_0.10.0_darwin_amd64.tar.gz"
-      sha256 "7f5fb374c5aec322e92d8d7171f20773d084bbf15b89da95e055c2e2d7b58d0d""
+      url "https://github.com/openclaw/discrawl/releases/download/v0.10.0/discrawl_0.10.0_darwin_amd64.tar.gz"
+      sha256 "7f5fb374c5aec322e92d8d7171f20773d084bbf15b89da95e055c2e2d7b58d0d"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-     url "https://github.com/openclaw/discrawl/releases/download/v0.10.0/discrawl_0.10.0_linux_arm64.tar.gz"
-      sha256 "8976f688a9a483e565b6da486e37015431122fa826186292ca079653586c7e37""
+      url "https://github.com/openclaw/discrawl/releases/download/v0.10.0/discrawl_0.10.0_linux_arm64.tar.gz"
+      sha256 "8976f688a9a483e565b6da486e37015431122fa826186292ca079653586c7e37"
     else
-     url "https://github.com/openclaw/discrawl/releases/download/v0.10.0/discrawl_0.10.0_linux_amd64.tar.gz"
-      sha256 "14a1e0b00f342dac7397ee1f8257979390e10a103ff8f162612cb9ea7004db9c""
+      url "https://github.com/openclaw/discrawl/releases/download/v0.10.0/discrawl_0.10.0_linux_amd64.tar.gz"
+      sha256 "14a1e0b00f342dac7397ee1f8257979390e10a103ff8f162612cb9ea7004db9c"
     end
   end
 
