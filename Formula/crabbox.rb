07f65c3ed6e7eb4170b6ef8eeaabb584ddab1932
Peter Steinberger <steipete@gmail.com>
2026-09-12 19:46:52 -0700

    refactor: simplify tap package definitions

--- Formula/crabbox.rb
+++ Formula/crabbox.rb
@@ -1,7 +1,6 @@
 # typed: false
 # frozen_string_literal: true
 
-# Maintained in this tap; the ordinary updater preserves install behavior.
 class Crabbox < Formula
   desc "Remote software testing and execution"
   homepage "https://github.com/openclaw/crabbox"
@@ -11,20 +10,10 @@ class Crabbox < Formula
     if Hardware::CPU.intel?
       url "https://github.com/openclaw/crabbox/releases/download/v0.57.0/crabbox_0.57.0_darwin_amd64.tar.gz"
       sha256 "84656eeba91916d7c23898a13d89c5247d356890e078822c818bbe8ce0101eee"
-
-      define_method(:install) do
-        bin.install "crabbox"
-        bin.install "crabbox-apple-vm-helper" if OS.mac? && Hardware::CPU.arm?
-      end
     end
     if Hardware::CPU.arm?
       url "https://github.com/openclaw/crabbox/releases/download/v0.57.0/crabbox_0.57.0_darwin_arm64.tar.gz"
       sha256 "bba898561b4f5548047088ee761f2c3c74574c85af68fde792196b9f9b33c217"
-
-      define_method(:install) do
-        bin.install "crabbox"
-        bin.install "crabbox-apple-vm-helper" if OS.mac? && Hardware::CPU.arm?
-      end
     end
   end
 
@@ -32,19 +21,16 @@ class Crabbox < Formula
     if Hardware::CPU.intel? && Hardware::CPU.is_64_bit?
       url "https://github.com/openclaw/crabbox/releases/download/v0.57.0/crabbox_0.57.0_linux_amd64.tar.gz"
       sha256 "3a395826da2fa7117149f160b25fc0586841664d3096754ca21bd85f1c7e9d37"
-      define_method(:install) do
-        bin.install "crabbox"
-        bin.install "crabbox-apple-vm-helper" if OS.mac? && Hardware::CPU.arm?
-      end
     end
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
       url "https://github.com/openclaw/crabbox/releases/download/v0.57.0/crabbox_0.57.0_linux_arm64.tar.gz"
       sha256 "1a0618a18d556ad9c601e8882b6dc80aabfaedd3f5a447bec20bb56409df82d3"
-      define_method(:install) do
-        bin.install "crabbox"
-        bin.install "crabbox-apple-vm-helper" if OS.mac? && Hardware::CPU.arm?
-      end
     end
+  end
+
+  def install
+    bin.install "crabbox"
+    bin.install "crabbox-apple-vm-helper" if OS.mac? && Hardware::CPU.arm?
   end
 
   test do
